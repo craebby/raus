1af0a75683538da93a98fd9151cdc26e53be0912
root <root@docker.fritz.box>
2026-09-18 10:25:48 +0000

    Edit ergaenzt und DB um Menge erweitert

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from database import init_db, add_item, get_today_count, get_items
+from database import get_db_version, get_item, init_db, add_item, get_today_count, get_items, update_item
 
 app = Flask(__name__)
 
@@ -22,11 +22,36 @@ def index():
 def add():
     name = request.form["name"].strip()
     action = request.form["action"]
+    quantity = int(request.form["quantity"])
 
     if name:
-        add_item(name, action)
+        add_item(quantity, name, action)
 
     return redirect("/")
+
+
+
+@app.route("/save/<int:item_id>", methods=["POST"])
+def save(item_id):
+    name = request.form["name"].strip()
+    action = request.form["action"]
+    quantity = int(request.form["quantity"])
+
+    if name:
+        update_item(item_id, quantity, name, action)
+
+    return redirect("/")
+
+
+@app.route("/edit/<int:item_id>", methods=["GET"])
+def edit(item_id):
+    edit_item = get_item(item_id)
+    print(edit_item)
+    return render_template(
+        "index.html",
+        edit_item=edit_item,
+    )
+
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import sqlite3
 from datetime import date
 
 DB_PATH = "data/raus.db"
+db_version = 1
 
 
 def get_connection():
@@ -18,20 +19,56 @@ def init_db():
                 created_at TEXT NOT NULL
             )
         """)
+        conn.commit()
+        actual_version = int(conn.execute("""PRAGMA user_version""").fetchone()[0])
+        if actual_version < db_version:
+            migrate_db(conn, actual_version, db_version)
 
+
+
+def migrate_db(conn, from_version, to_version):
+    if from_version < 1 <= to_version:
+        # Example migration for version 1
+        conn.execute("""
+            ALTER TABLE items ADD COLUMN quantity INTEGER DEFAULT 0
+        """)
+        conn.commit()
+        conn.execute(f"PRAGMA user_version = {to_version}")
         conn.commit()
 
+def get_db_version():
+    with get_connection() as conn:
+        result = conn.execute(
+            """
+            PRAGMA user_version
+            """
+        ).fetchone()
 
-def add_item(name, action):
+    return result[0]
+
+
+def add_item(quantity, name, action):
     with get_connection() as conn:
         conn.execute(
             """
-            INSERT INTO items (name, action, created_at)
-            VALUES (?, ?, ?)
+            INSERT INTO items (quantity, name, action, created_at)
+            VALUES (?, ?, ?, ?)
             """,
-            (name, action, date.today().isoformat()),
+            (quantity, name, action, date.today().isoformat()),
         )
 
+        conn.commit()
+
+def update_item(item_id, quantity, name, action):
+    with get_connection() as conn:
+        conn.execute(
+            """
+            UPDATE items
+            SET quantity = ?, name = ?, action = ?
+            WHERE id = ?
+            """,
+            (quantity, name, action, item_id),
+        )
         conn.commit()
 
 
@@ -53,9 +90,21 @@ def get_items():
     with get_connection() as conn:
         return conn.execute(
             """
-            SELECT name, action, created_at
+            SELECT quantity, name, action, created_at, id
             FROM items
             ORDER BY id DESC
             LIMIT 20
             """
         ).fetchall()
+
+def get_item(item_id):
+    with get_connection() as conn:
+        return conn.execute(
+            """
+            SELECT quantity, name, action, created_at, id
+            FROM items
+            WHERE id = ?
+            """, (item_id,),
+        ).fetchone()
+    
+
